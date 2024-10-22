6c7fa6850811f1c52d80b99fd8a09cd57e27bc11
Tisaac1 <lashay_isaac@yahoo.com>
2024-10-22 14:12:05 -0400

    update

---
+++ newfile/hellofile/something.mjs
@@ -0,0 +1,14 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+
+// User Editable Region
+
+rows[2] = 10;
+rows[2][rows.length - 1]; 
+let length = rows.history.length;
+
+// User Editable Region
+
+console.log(rows);
